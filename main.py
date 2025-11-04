d2a49b186b1bc7dba2c91fad6a2a653a9068b555
João Gilberto <84871727+Trissmegisto@users.noreply.github.com>
2025-11-04 18:48:32 -0300

    Início

---
+++ main.py
@@ -0,0 +1,3 @@
+# Entrada: 1. número de pilhas
+# 2. Número de blocos em cada pilha, em ordem crescente
+# Saída: maior triãngulo isosceles possível usandos os blocos, retirando os blocos que julgar desnecessário.
